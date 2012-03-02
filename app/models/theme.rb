54c5a2739319bfac3c7b8d2c11a391c060ee6afb
Imre Mehesz <imehesz@imehesz-laptop.(none)>
2012-03-02 08:51:47 -0500

    updated theme model to use cool file size validator

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,4 +1,21 @@
 class Theme < ActiveRecord::Base
+ 	require 'file_size_validator'
+
 	mount_uploader :screenshot, FileUploader
 	mount_uploader :file, FileUploader
+
+	validates :name,
+		:presence => true
+	
+	validates :screenshot,
+		:presence => true,
+		:file_size => {
+			:maximum => 2.megabytes.to_i
+		}
+	
+	validates :file,
+		:presence => true,
+		:file_size => {
+			:maximum => 2.megabytes.to_i
+		}
 end
